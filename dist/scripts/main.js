430b30128a66207c4807613deaaf16cd7cca5aed
Administrator <administrator@MacBook-Pro.local>
2018-02-05 23:02:19 +0000

    we dont need this files any more

--- dist/scripts/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var greet_1 = require("./greet");
-var showHello = function (divName, name) {
-    var elt = document.getElementById(divName);
-    elt.innerText = greet_1.sayHello(name);
-};
-showHello("greeting", "Typescript");
